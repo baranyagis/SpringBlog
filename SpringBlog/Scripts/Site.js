0897be4652a4edf00ab5a4588e6b088efdf2e527
Baran Kızmaz <baranyagis2@hotmail.com>
2020-05-28 16:56:00 +0300

    Spring Blog

---
+++ SpringBlog/Scripts/Site.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+    // https://getbootstrap.com/docs/4.4/components/tooltips/
+
+    $('[data-toggle="tooltip"]').tooltip();
+
+    bsCustomFileInput.init();
+
+    $("#frmSearch").submit(function (e) {
+        var q = $("#q").val().trim();
+
+        $("#q").val(q);
+
+        if (!q) {
+            e.preventDefault();
+        }
+    });
+});
